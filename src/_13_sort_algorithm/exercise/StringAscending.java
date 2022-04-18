06c146c5212571541cf78c0068cfb495dedc4eb2
Thaouyen138 <96711178+Thaouyen138@users.noreply.github.com>
2022-04-18 13:32:02 +0700

    something

---
+++ src/_13_sort_algorithm/exercise/StringAscending.java
@@ -0,0 +1,32 @@
+package _13_sort_algorithm.exercise;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StringAscending {
+
+       public static void findStringAscending() {
+           Scanner scanner=new Scanner(System.in);
+           System.out.println("enter string");
+           String string=scanner.nextLine();
+           LinkedList<Character> list = new LinkedList<>();
+           list.add(string.charAt(0));
+           for (int i = 0; i < string.length(); i++) {
+               if (list.getLast() < string.charAt(i)) {
+                   list.add(string.charAt(i));
+
+               }
+           }
+           for (Character c : list) {
+               System.out.print(c);
+           }
+       }
+    public static void main(String[] args) {
+           findStringAscending();
+
+        }
+    }
+
