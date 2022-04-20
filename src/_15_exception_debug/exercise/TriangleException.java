bb7c934508341ccac83dfc5d6fa3d38500f74e12
Thaouyen138 <96711178+Thaouyen138@users.noreply.github.com>
2022-04-20 16:10:52 +0700

    something

--- src/_15_exception_debug/exercise/TriangleException.java
+++ src/_15_exception_debug/exercise/TriangleException.java
@@ -1,27 +1,31 @@
 package _15_exception_debug.exercise;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TriangleException {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        try {
-//            System.out.print("enter size 1:");
-//            double size1 = Double.parseDouble(scanner.nextLine());
-//            System.out.print("enter size 2:");
-//            double size2 = Double.parseDouble(scanner.nextLine());
-//            System.out.print("enter size 3:");
-//            double size3 = Double.parseDouble(scanner.nextLine());
-//            if (size1 < 0 || size2 < 0 || size3 < 0) {
-//                throw new IllegalTriangleExceptions("negative numbers,is not triangle");
-//
-//            }
-//            if (size1+size2<size3|| size1+size3<size2|| size2+size3<size1){
-//                throw  new IllegalTriangleExceptions("total size is small,is not triangle");
-//            }
-//        } catch (IllegalTriangleExceptions illegalTriangleExceptions) {
-////            illegalTriangleExceptions.printStackTrace();
-//            System.out.println(illegalTriangleExceptions.getMessage());
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("enter size 1:");
+            double size1 = Double.parseDouble(scanner.nextLine());
+            System.out.print("enter size 2:");
+            double size2 = Double.parseDouble(scanner.nextLine());
+            System.out.print("enter size 3:");
+            double size3 = Double.parseDouble(scanner.nextLine());
+            if (size1 < 0 || size2 < 0 || size3 < 0) {
+                throw new IllegalTriangleExceptions("negative numbers,is not triangle");
+
+            }
+            if (size1+size2<size3|| size1+size3<size2|| size2+size3<size1){
+                throw  new IllegalTriangleExceptions("total size is small,is not triangle");
+            }
+        } catch (IllegalTriangleExceptions illegalTriangleExceptions) {
+//            illegalTriangleExceptions.printStackTrace();
+            System.out.println(illegalTriangleExceptions.getMessage());
+        }catch(NumberFormatException numberFormatException){
+            System.out.println("wrong format!");
         }
-//    }
+
+    }
 }
